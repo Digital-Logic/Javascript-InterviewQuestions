45da703c806d7e2587434f11ebf0984355232c8e
lbrown <lbrown@digital-logic.net>
2021-11-10 09:02:51 -0700

    Update Solution

--- src/capitalize.js
+++ src/capitalize.js
@@ -4,13 +4,26 @@
  * Input: "can i help you?", Output: "Can I Help You?"
  */
 
-function capitalizeWords(str) {
+
+function capitalize(str) {
+    const result = str.split(" ");
+
+    for (let i=0; i<result.length; ++i)
+        result[i] = result[i].slice(0,1).toUpperCase()
+                        + result[i].slice(1);
+
+    return result.join(" ");
+}
+
+
+function capitalizeTwo(str) {
     return str.split(" ")
-        .map(c => c.slice(0,1).toUpperCase() + c.slice(1))
+        .map(w => w.slice(0,1).toUpperCase() + w.slice(1))
         .join(" ");
 }
 
 
 module.exports = {
-    capitalizeWords
+    capitalize,
+    capitalizeTwo
 };
