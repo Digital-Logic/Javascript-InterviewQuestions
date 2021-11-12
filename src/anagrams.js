63390c55f1d70db50d14cdf1fe695174e213ce84
lbrown <lbrown@digital-logic.net>
2021-11-12 10:33:49 -0700

    Updated test cases

--- src/anagrams.js
+++ src/anagrams.js
@@ -2,12 +2,12 @@
  * Given two strings, write a function that will determine if one string is
  * an anagram of another. An anagram is a string that is formed by rearranging
  * the letters of another string.
- * Ignore capitalization, and spaces. Punctuation will not be included in the provided strings.
+ * Ignore capitalization, punctuation, and spaces.
  *
  * Examples
  * "Arc" is an anagram of "Car"
  * "Debit card" is an anagram of "Bad credit"
- * "Astronomer" is an anagram of "Moon starer"
+ * "Astronomer!" is an anagram of "Moon starer"
  */
 
 function isAnagram(strA, strB) {
