c4c17e691adb9b959cb1e5c3b6ac12ab9e71fd7b
lbrown <lbrown@digital-logic.net>
2021-11-24 15:27:28 -0700

    Update Solution

--- src/matrixRotation.js
+++ src/matrixRotation.js
@@ -1,7 +1,8 @@
 
 /**
- * 1. Write a function that will rotate an NxN matrix clockwise by 90 degrees.
- *    The matrix is guaranteed to be square.
+ * Write a function that will rotate an NxN matrix clockwise by 90 degrees.
+ * The matrix is guaranteed to be square.
+ *
  * Examples
  * [[1,2], => [[3,1],
  *  [3,4]]     [4,2]]
@@ -10,7 +11,7 @@
  *   [4,5,6], =>  [8,5,2],
  *   [7,8,9]]     [9,6,3]]
  *
- *  2. Can you rotate the matrix in-place?
+ *  Extra Credit: Can you rotate the matrix in-place?
  **/
 
 function rotateMatrix(grid) {
